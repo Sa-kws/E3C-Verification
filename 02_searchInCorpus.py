924fa717b1860604647e1a027b829b1a05105c0a
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-11-05 13:14:00 +0100

    Add files via upload

--- 02_searchInCorpus.py
+++ 02_searchInCorpus.py
@@ -9,6 +9,12 @@ FOLDER = 'Underscored_datas'
 words_to_find = str(input('Mots à chercher ? [si plusieurs, séparer par un underscore "_"]')).lower().split('_')
 print('Les mots à chercher sont :', words_to_find)
 
+csv = input('Sortie csv ? [y] ou [n]').lower()
+csv = True if csv == 'y' else False
+
+
+founded_words = []
+
 for file in os.listdir(FOLDER):
     name_of_infile = FOLDER + '/' + file
     sentences = {}
@@ -36,15 +42,42 @@ for file in os.listdir(FOLDER):
 
                 for key, value in sentences.items():
                     if position_start >= int(value[2]) and position_end <= int(value[3]):
-                        print(word_compt, word, key)
+                        contexte_g = 20
+                        try:
+                            while document[position_start-contexte_g] != ' ' or document[position_start-contexte_g] != '\n':
+                                 contexte_g += 1
+                        except IndexError:
+                            contexte_g = 0
+                        contexte_d = 20
+                        try:
+                            while document[position_start+contexte_d] != ' ' or document[position_start-contexte_d] != '\n':
+                                 contexte_d += 1
+                        except IndexError:
+                            contexte_d = 0
+                        founded_words.append([word,
+                        file,
+                        key,
+                        position_start,
+                        position_end,
+                        document[position_start-contexte_g:position_end],
+                        document[position_start:position_end+contexte_d],
+                        document[int(value[2]):int(value[3])]])
                 word_compt += 1
 
                 document = document.replace(word + ' ', '#'*len(word) + ' ', 1)
 
-                #print(word, file)
-
-
     break
+
+if csv == True:
+    with open('Searched_words_result.csv', 'w', encoding='utf-8') as csv_file:
+        csv_file.write('Word\tfile\tSentence_number\tWord_start_position\tWord_end_position\tcontexte_gauche\tcontexte_droit\tSentence\n')
+        for line in founded_words:
+            csv_file.write(str(line[0]) + '\t' + str(line[1]) + '\t' + str(line[2]) +
+            '\t' + str(line[3]) + '\t' + str(line[4]) + '\t' + str(line[5]) +  '\t' + str(line[6]) + '\t' + str(line[7]) +  '\n')
+    print('CSV file created as "Searched_words_result.csv".')
+else:
+    for line in founded_words:
+        print(line)
 
 # Il faut une liste de mots à rechercher et
 # partir de cette liste pour faire un t in s et
