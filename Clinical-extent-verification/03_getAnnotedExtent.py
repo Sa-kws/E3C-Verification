3d0845c3069efa2c797007fe272b15d41300c9ed
Sa-kws <77007516+Sa-kws@users.noreply.github.com>
2021-11-18 15:37:10 +0100

    Update 03_getAnnotedExtent.py

--- Clinical-extent-verification/03_getAnnotedExtent.py
+++ Clinical-extent-verification/03_getAnnotedExtent.py
@@ -20,25 +20,42 @@ for file in os.listdir(FOLDER):
 
     tree = etree.parse(name_of_infile)
 
-    for element in tree.xpath(CLINENTITY_path):
-        dictionnary_index += 1
-        CLINENTITY_extent[dictionnary_index] = [int(element.attrib['begin']), int(element.attrib['end']), element.attrib['entityID']]
-
     for element in tree.xpath(document_path):
         document = element.attrib['sofaString']
+
+    for element in tree.xpath(CLINENTITY_path):
+        dictionnary_index += 1
+        try:
+            words_code[element.attrib['entityID']] = document[int(element.attrib['begin']):int(element.attrib['end'])]
+            CLINENTITY_extent[dictionnary_index] = [int(element.attrib['begin']), int(element.attrib['end']), element.attrib['entityID']]
+        except KeyError:
+            CLINENTITY_extent[dictionnary_index] = [int(element.attrib['begin']), int(element.attrib['end']), 'NO_entityID']
+
 
     #for element in tree.xpath(sentence_path):
     #    print(element.attrib['begin'], element.attrib['end'])
 
-    break
+    print(file, '------\tdone\t✅')
+
+    #break
 
 for key, value in CLINENTITY_extent.items():
-    words_code[value[2]] = document[value[0]:value[1]]
     if value[2] not in CLINENTITY_extent_repartition:
         CLINENTITY_extent_repartition[value[2]] = 1
     else:
         CLINENTITY_extent_repartition[value[2]] += 1
+
 CLINENTITY_extent_repartition = dict(sorted(CLINENTITY_extent_repartition.items(), key=lambda x: x[1], reverse=True))
 
-print(CLINENTITY_extent_repartition)
-print(words_code)
+with open('10_CLINENTITY_extent_repartition.txt', 'w', encoding='utf-8') as repartition_outfile:
+    for key, value in CLINENTITY_extent_repartition.items():
+        repartition_outfile.write(key + '\t' + str(value) + '\n')
+
+with open('11_words_code.txt', 'w', encoding='utf-8') as code_outfile:
+    for key, value in words_code.items():
+        code_outfile.write(key + '\t' + value + '\n')
+
+end = time.time()
+temps = end-begin
+minutes = round((temps / 60),2)
+print('Temps d\'execution : '+str(minutes)+' minute.s.')
